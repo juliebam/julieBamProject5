74b128b2824249a86310adf9c0ff22e24f0f47d5
Julie <julie.bam1985@gmail.com>
2019-02-28 16:36:26 -0500

    made the search bar functional

--- src/App.js
+++ src/App.js
@@ -1,28 +1,106 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Search from './Search';
+import axios from 'axios';
+
+
+// const apiURL = "https://api.edamam.com/search";
+const apiKey = "4d616a3fe66b009185305fec33387408";
+const apiId = "0766a929";
+
 
 class App extends Component {
+constructor(){
+  super();
+  this.state ={
+    userInput: '',
+    userName: 'Julie'
+
+  }
+}
+  
+  componentDidMount(){
+//     axios({
+//       method:'get',
+//       url: "https://api.edamam.com/search",
+//       responseType:'jsonp',
+//         params:
+//           {
+//             q: this.state.userInput,
+//             app_id: apiId,
+//             app_key: apiKey,
+//             format:'json',
+//             to: 20,
+//             diet: 'low-carb'
+//           }
+//    }).then(response =>{
+//    response = response.data.hits
+//   //  console.log(response)
+//   //  this.setState({
+//   //    art:response
+//   //  })
+//  })
+} 
+
+//every time the user type his input, this input will be saved into this state below.
+handleChange = (event) => {
+  this.setState({
+    userInput: event.target.value
+  })
+}
+
+handleSubmit =(event) => {
+  event.preventDefault();
+  const userInputString = this.state.userInput;    
+  axios({
+      method:'get',
+      url: "https://api.edamam.com/search",
+      responseType:'jsonp',
+      params:
+        {
+          q: userInputString,
+          app_id: apiId,
+          app_key: apiKey,
+          format:'json',
+          to: 20,
+          diet: 'low-carb'
+        }
+}).then(response => {
+    response = response.data.hits
+    console.log(response)
+    this.setState({
+     recipes: response
+ })
+})
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <main>
+          {/* <Search name={this.state.userName} onClick={this.handleSubmit} change={this.handleChange}/>  */}
+        
+          <section className="App-form">
+            <h1>{this.props.name}</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="search">Enter your main ingredient</label>
+                    <input type="text" id="search" placeholder="i.e. chicken" required onChange={this.handleChange} value={this.state.userIput}/>
+                    <input type="submit" value="submit" />
+                </form>
+            </section> 
+        </main>
+        
       </div>
     );
   }
 }
 
 export default App;
+
+// need to link the search field to the axios... so the userInput  goes as a query in the axios call
+//need to get the results from the axios call displayed on the page
+// what will be displayed: image and label, take me to the recipe button/a tag will redirect user to the url with the actual recipe
+
+//onSubmit - get the user's Input and 
---
+++ src/Search.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    handleChange = (event) => {
+        console.log(event.target.value)
+      }
+      
+    render(props){
+        return(
+            <section className="App-form">
+            <h1>{this.props.name}</h1>
+                <form onSubmit={this.props.onClick}>
+                    <label htmlFor="search">Enter your main ingredient</label>
+                    <input type="text" id="search" placeholder="i.e. chicken" required onChange={this.handleChange}/>
+                    <input type="submit" value="submit" />
+                </form>
+            </section> 
+        )
+    }
+}
+
+export default Search;
+
+
+
